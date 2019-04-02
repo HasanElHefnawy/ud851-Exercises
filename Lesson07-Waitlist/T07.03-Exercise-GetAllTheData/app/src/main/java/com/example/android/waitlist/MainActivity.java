3bfcc3cf352d28dd84fdcd3647997293f2e1f0b7
Hasan El-Hefnawy <helhefnawy89@yahoo.com>
2019-04-02 14:19:29 +0200

    T07.03-Exercise-GetAllTheData

--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.android.waitlist;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import com.example.android.waitlist.data.TestUtil;
+import com.example.android.waitlist.data.WaitlistDbHelper;
+import com.example.android.waitlist.data.WaitlistContract.*;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private GuestListAdapter mAdapter;
 
     // TODO (1) Create a local field member of type SQLiteDatabase called mDb
+    SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +34,21 @@ public class MainActivity extends AppCompatActivity {
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Create an adapter for that cursor to display the data
-        mAdapter = new GuestListAdapter(this);
-
         // TODO (2) Create a WaitlistDbHelper instance, pass "this" to the constructor as context
+        WaitlistDbHelper mDbHelper = new WaitlistDbHelper(this);
 
         // TODO (3) Get a writable database reference using getWritableDatabase and store it in mDb
+        mDb = mDbHelper.getWritableDatabase();
 
         // TODO (4) call insertFakeData from TestUtil and pass the database reference mDb
+        TestUtil.insertFakeData(mDb);
 
         // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
+        Cursor cursor = getAllGuests();
 
         // TODO (12) Pass the resulting cursor count to the adapter
+        // Create an adapter for that cursor to display the data
+        mAdapter = new GuestListAdapter(this, cursor.getCount());
 
         // Link the adapter to the RecyclerView
         waitlistRecyclerView.setAdapter(mAdapter);
@@ -54,7 +65,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (5) Create a private method called getAllGuests that returns a cursor
+    private Cursor getAllGuests() {
 
     // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
+        String[] projection = {
+                BaseColumns._ID,
+                WaitlistEntry.COLUMN_GUEST_NAME,
+                WaitlistEntry.COLUMN_PARTY_SIZE,
+                WaitlistEntry.COLUMN_TIMESTAMP
+        };
+        String sortOrder = WaitlistEntry.COLUMN_TIMESTAMP + " DESC";
+        Cursor cursor = mDb.query(
+                WaitlistEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+        return cursor;
+    }
 
 }
